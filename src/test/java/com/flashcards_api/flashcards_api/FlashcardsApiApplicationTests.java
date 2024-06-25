cf2ad151809e9176550779433f1031e0ab3486ff
ArmandoBraho <105724648+ArmandoBraho@users.noreply.github.com>
2024-06-25 19:09:25 +0200

    initialize flaschard-api repo

---
+++ src/test/java/com/flashcards_api/flashcards_api/FlashcardsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.flashcards_api.flashcards_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlashcardsApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
